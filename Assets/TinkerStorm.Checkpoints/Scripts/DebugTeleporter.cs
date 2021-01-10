582694967bd3f88017be836d0f97985a4b25e22c
Junior <sudojunior@outlook.com>
2021-01-10 23:06:08 +0000

    Add debug teleporter

---
+++ Assets/TinkerStorm.Checkpoints/Scripts/DebugTeleporter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TinkerStorm.Checkpoints
+{
+    public class DebugTeleporter : MonoBehaviour
+    {
+        [SerializeField]
+        Transform destination;
+
+        bool isTrigger;
+
+        void Start()
+        {
+            isTrigger = GetComponent<Collider>().isTrigger;
+        }
+
+        void Run(Transform other)
+        {
+            Respawnable respawner = other.GetComponent<Respawnable>();
+            if(respawner != null)
+            {
+                respawner.SetRespawnAt(destination);
+                respawner.Respawn();
+            }
+        }
+
+        void OnCollisionEnter(Collision other)
+        {
+            if (!isTrigger)
+                Run(other.transform);
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (isTrigger)
+                Run(other.transform);
+        }
+    }
+}
